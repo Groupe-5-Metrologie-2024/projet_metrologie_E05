901bfc5288addb1c291f4a71ead029f5ee56ada4
AlexisD-V <alexis.dube-valade@polymtl.ca>
2024-04-03 09:38:59 -0400

    fk

--- Fonction sympy rho analytique.py
+++ Fonction sympy rho analytique.py
@@ -9,8 +9,8 @@ from scipy.integrate import quad
 from sympy import print_latex
 from scipy.constants import mu_0
 from scipy.constants import epsilon_0
-import matplotlib.pyplot as plt
-import numpy as np
+
+
 b, xi, omega, mur, mu0, rho, e0, er, a ,n, r = sp.symbols("b,\\xi, \\omega, \\mu_r,\\mu_0,\\rho, \\epsilon_0,\\epsilon_r,a,n,\\infty")
 
 gammaprint = b*(xi**2+1j*omega*mur*mu0/rho-omega**2*mu0*mur*e0*er)**(1/2)
@@ -19,29 +19,28 @@ etaprint = (xi**2-omega**2*mu0*e0)**(1/2)
 def f(xi):
     gamma = b*(xi**2+1j*omega*mur*mu0/rho-omega**2*mu0*mur*e0*er)**(1/2)
     eta = (xi**2-omega**2*mu0*e0)**(1/2)
-    f = 
     return sp.sin(xi/2)**2/(xi/2)**2*(mur*eta*b*I(0,eta*b)*I(1,gamma)-gamma*I(1,eta*b)*I(0,gamma))/(mur*eta*b*K(0,eta*b)*I(1,gamma)+gamma*K(1,eta*b)*I(0,gamma))*K(1,eta*a)**2
 
 z = 1j*omega*mu0*a**2*n**2*sp.Integral(f(xi), (xi,-r,r))
-"""zprime = sp.diff(z, rho)
+zprime = sp.diff(z, rho)
 
-marde = z/zprime"""
+marde = z/zprime
 
-"""z_result = z.evalf(5,subs={b:1.58*10**(-3),omega:2*np.pi*100*10**3,mur:1,mu0:mu_0,e0:epsilon_0,er:1,a:10.43*10**(-3),n:1,rho:820*10**(-9),r:0.1})
-print(z_result)"""
-
-x = 1
-y= f(x)
-y_real = np.real(y)
-print(y_real)
+rho_result = zprime.evalf(5,subs={b:1.58*10**(-3),omega:2*np.pi*100*10**3,mur:1,mu0:mu_0,e0:epsilon_0,er:1,a:10.43*10**(-3),n:1,rho:820*10**(-9),r:np.inf})
+print(rho_result)
 
 
-
-
-
-
-
-
+"""print_latex(z)
+print("done")
+print_latex(zprime)
+print("done")
+print_latex(marde)
+print("eta")
+print_latex(etaprint)
+print("gamma")
+print_latex(gammaprint)
+print("gammaprime")
+print_latex(sp.diff(gammaprint,rho))"""
 
 
 
